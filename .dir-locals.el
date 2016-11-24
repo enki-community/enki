413275b8ae4ce305ded645793eb0b3cd964997f6
Mathieu Lirzin <mathieu.lirzin@etu.u-bordeaux.fr>
2016-11-24 20:44:52 +0100

    maint: Let Emacs configure itself.
    Use directory local variables instead of encouraging contributors to define a
    custom "enki" style.

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (fill-column . 80))
+ (c++-mode
+  (c-file-style . "bsd")
+  (c-basic-offset . 4)
+  (tab-width . 4)))
