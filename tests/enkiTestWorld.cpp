5435915abaceda2a7e7c83107bcdf06a76abbc62
Sebastien Pouteau <sebastien.pouteau@etu.u-bordeaux.fr>
2017-01-09 17:25:13 +0100

    tests: Add tests for Thymio2, S-bot, EPuck, Marxbot, Khepera robots.

--- tests/enkiTestWorld.cpp
+++ tests/enkiTestWorld.cpp
@@ -20,6 +20,11 @@
 #include "catch.hpp"
 
 #include <enki/PhysicalEngine.h>
+#include <enki/robots/e-puck/EPuck.h>
+#include <enki/robots/thymio2/Thymio2.h>
+#include <enki/robots/s-bot/Sbot.h>
+#include <enki/robots/khepera/Khepera.h>
+#include <enki/robots/marxbot/Marxbot.h>
 
 // XXX: Use BDD style.
 TEST_CASE( "An empty Enki world", "[Enki::World]" ) {
@@ -91,5 +96,164 @@ TEST_CASE( "An empty Enki world", "[Enki::World]" ) {
 		REQUIRE( world.bluetoothBase == NULL );
 		REQUIRE( world.wallsType == Enki::World::WALLS_CIRCULAR );
 		REQUIRE( world.objects.size() == 0 );
+	}
+}
+
+using namespace Enki;
+
+TEST_CASE( "Thymio2", "[Enki::Thymio2]" ) {
+	SECTION( "The default constructor" ) {
+		Thymio2 thymio;
+
+		// Test color of robot
+		for (unsigned int i=0; i< Thymio2::LED_COUNT; i++)
+		{
+			switch(i)
+			{
+			case Thymio2::TOP:
+			case Thymio2::BOTTOM_LEFT:
+			case Thymio2::BOTTOM_RIGHT:
+				REQUIRE( thymio.getColorLed((Thymio2::LedIndex)i) == Color(0.0,0.0,0.0,0.0) );
+				break;
+
+			case Thymio2::BUTTON_UP:
+			case Thymio2::BUTTON_DOWN:
+			case Thymio2::BUTTON_LEFT:
+			case Thymio2::BUTTON_RIGHT:
+				REQUIRE( thymio.getColorLed((Thymio2::LedIndex)i)== Color(1.0,0.0,0.0,0.0) );
+				break;
+
+			case Thymio2::RING_0: case Thymio2::RING_1: case Thymio2::RING_2:
+			case Thymio2::RING_3: case Thymio2::RING_4: case Thymio2::RING_5:
+			case Thymio2::RING_6: case Thymio2::RING_7:
+				REQUIRE( thymio.getColorLed((Thymio2::LedIndex)i)== Color(1.0,0.5,0.0,0.0) );
+				break;
+
+			case Thymio2::IR_FRONT_0: case Thymio2::IR_FRONT_1: case Thymio2::IR_FRONT_2:
+			case Thymio2::IR_FRONT_3: case Thymio2::IR_FRONT_4: case Thymio2::IR_FRONT_5:
+			case Thymio2::IR_BACK_0:  case Thymio2::IR_BACK_1:
+				REQUIRE( thymio.getColorLed((Thymio2::LedIndex)i)== Color(1.0,0.0,0.0,0.0) );
+				break;
+
+			case Thymio2::LEFT_BLUE: case Thymio2::RIGHT_BLUE:
+				REQUIRE( thymio.getColorLed((Thymio2::LedIndex)i)== Color(0.0,1.0,1.0,0.0) );
+				break;
+
+			case Thymio2::LEFT_RED:	 case Thymio2::RIGHT_RED:
+				REQUIRE( thymio.getColorLed((Thymio2::LedIndex)i)== Color(1.0,0.0,0.0,0.0) );
+				break;
+
+			default: break;
+			}
+		}
+	}
+
+	SECTION( "position and color" ) {
+		Thymio2 thymio;
+		thymio.pos = Point(10, 10);
+
+		REQUIRE( thymio.pos.x == 10 );
+		REQUIRE( thymio.pos.y == 10 );
+
+		for (unsigned int i = 0; i < Thymio2::LED_COUNT; i++)
+		{
+			thymio.setLedColor((Thymio2::LedIndex) i, Color(10.0, 10.0, 10.0, 1.0));
+			if (i == Thymio2::TOP || i == Thymio2::BOTTOM_LEFT || i == Thymio2::BOTTOM_RIGHT)
+			{
+				REQUIRE( thymio.getColorLed((Thymio2::LedIndex) i) == Color(10.0, 10.0, 10.0, 1.0) );
+			}
+			else
+			{
+				Color c = Color(thymio.getColorLed((Thymio2::LedIndex) i).r(),thymio.getColorLed((Thymio2::LedIndex) i).g(),thymio.getColorLed((Thymio2::LedIndex)i).b(),1.0);
+				REQUIRE( thymio.getColorLed((Thymio2::LedIndex) i) ==  c );
+			}
+		}
+	}
+}
+
+TEST_CASE( "EPuck", "[Enki::EPuck]" ) {
+	SECTION( "The default constructor" ) {
+		EPuck ePuck;
+
+		REQUIRE( ePuck.pos.x == 0 );
+		REQUIRE( ePuck.pos.y == 0 );
+		REQUIRE( ePuck.getColor() == Color(0, 0.7, 0) );
+		REQUIRE( ePuck.isCylindric() );
+		REQUIRE( ePuck.getHeight() == 4.7 );
+		REQUIRE( ePuck.getRadius() == 3.7 );
+	}
+
+	SECTION( "Position and color" ) {
+		EPuck ePuck;
+
+		ePuck.pos = Point(10,10);
+		ePuck.setLedRing(true);
+
+		REQUIRE( ePuck.pos.x == 10 );
+		REQUIRE( ePuck.pos.y == 10 );
+		REQUIRE( ePuck.getColor() == Color::red);
+	}
+}
+
+TEST_CASE( "Marxbox", "[Enki::Marxbox]" ) {
+	SECTION( "The default constructor" ) {
+		Marxbot marxbot;
+
+		REQUIRE( marxbot.pos.x == 0 );
+		REQUIRE( marxbot.pos.y == 0 );
+		REQUIRE( marxbot.isCylindric() );
+		REQUIRE( marxbot.getHeight() == 12 );
+		REQUIRE( marxbot.getRadius() == 8.5 );
+	}
+
+	SECTION( "Position" ) {
+		Marxbot marxbot;
+
+		marxbot.pos = Point(10,10);
+
+		REQUIRE( marxbot.pos.x == 10 );
+		REQUIRE( marxbot.pos.y == 10 );
+	}
+}
+
+TEST_CASE( "Khepera", "[Enki::Khepera]" ) {
+	SECTION( "The default constructor" ) {
+		Khepera khepera;
+
+		REQUIRE( khepera.pos.x == 0 );
+		REQUIRE( khepera.pos.y == 0 );
+		REQUIRE( khepera.isCylindric() );
+		REQUIRE( khepera.getHeight() == 5 );
+		REQUIRE( khepera.getRadius() == 2.6 );
+	}
+
+	SECTION( "Position" ) {
+		Khepera khepera;
+
+		khepera.pos = Point(10,10);
+
+		REQUIRE( khepera.pos.x == 10 );
+		REQUIRE( khepera.pos.y == 10 );
+	}
+}
+
+TEST_CASE( "S-Bot", "[Enki::Sbot]" ) {
+	SECTION( "The default constructor" ) {
+		Sbot sbot;
+
+		REQUIRE( sbot.pos.x == 0 );
+		REQUIRE( sbot.pos.y == 0 );
+		REQUIRE( sbot.isCylindric() );
+		REQUIRE( sbot.getHeight() == 15 );
+		REQUIRE( sbot.getRadius() == 6 );
+	}
+
+	SECTION( "Position" ) {
+		Sbot sbot;
+
+		sbot.pos = Point(10,10);
+
+		REQUIRE( sbot.pos.x == 10 );
+		REQUIRE( sbot.pos.y == 10 );
 	}
 }
