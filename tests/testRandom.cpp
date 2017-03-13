19cb8a88e6fd01c02c8be458ef7a46e25893fd05
Nicolas Palard <nicpalard@etu.u-bordeaux.fr>
2017-03-13 14:09:36 +0100

    test to delete later

--- tests/testRandom.cpp
+++ tests/testRandom.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include "WorldGenerator.h"
 #include "Randomizer2.h"
+#include <typeinfo>
 
 using namespace Enki;
 int main()
 {
-	WorldGenerator* gen = new WorldGenerator();
-	gen->addRobots();
+	WorldGenerator* gen = new WorldGenerator;
+	gen->add(-1);
 	World* w = gen->getWorld();
 	Randomizer rd(w);
 	Robot* r = rd.generateThymio();
 	std::cout << "World type " << w->wallsType << " & objects number " << w->objects.size() << std::endl;
+	for(auto s : w->objects)
+		{
+			std::cerr << typeid(*s).name() << std::endl;
+		}
 
 }
