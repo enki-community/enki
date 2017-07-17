6a4858ebe096f391f0159103bdade3866aac4aa9
Stéphane Magnenat <stephane@magnenat.net>
2017-07-17 09:38:14 +0200

    Attempt to solve Win32 Polygon name clash problem

--- examples/playground/Playground.cpp
+++ examples/playground/Playground.cpp
@@ -36,6 +36,9 @@
 #include <enki/robots/e-puck/EPuck.h>
 #include <enki/robots/marxbot/Marxbot.h>
 #include <enki/robots/thymio2/Thymio2.h>
+#ifdef WIN32
+#define NOGDI // to avoid namespace clash between Win32 Polygon function and Enki class
+#endif // WIN32
 #include <QApplication>
 #include <QtGui>
 #include <iostream>
